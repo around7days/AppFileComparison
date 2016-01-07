f8ffdb782e047635133bbfb3c4dcd6c582c7f3a4
panasonic <panasonic@10.254.21.156>
2016-01-07 13:26:06 +0900

    commit

---
+++ src/main/DiffListBean.java
@@ -0,0 +1,133 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import main.FileComparisonConst.CompareKbn;
+
+import org.apache.log4j.Logger;
+
+/**
+ * 比較結果格納Bean
+ * @author 7days
+ */
+public class DiffListBean {
+
+    /** Logger */
+    @SuppressWarnings("unused")
+    private static final Logger logger = Logger.getLogger(DiffListBean.class);
+
+    /** 比較元ディレクトリ */
+    private String compareDir1;
+    /** 比較先ディレクトリ */
+    private String compareDir2;
+
+    /** 比較結果格納リスト */
+    private List<DiffBean> diffList = new ArrayList<>();
+
+    /**
+     * 比較元ディレクトリを取得します。
+     * @return 比較元ディレクトリ
+     */
+    public String getCompareDir1() {
+        return compareDir1;
+    }
+
+    /**
+     * 比較元ディレクトリを設定します。
+     * @param compareDir1 比較元ディレクトリ
+     */
+    public void setCompareDir1(String compareDir1) {
+        this.compareDir1 = compareDir1;
+    }
+
+    /**
+     * 比較先ディレクトリを取得します。
+     * @return 比較先ディレクトリ
+     */
+    public String getCompareDir2() {
+        return compareDir2;
+    }
+
+    /**
+     * 比較先ディレクトリを設定します。
+     * @param compareDir2 比較先ディレクトリ
+     */
+    public void setCompareDir2(String compareDir2) {
+        this.compareDir2 = compareDir2;
+    }
+
+    /**
+     * 比較結果の格納
+     * @param diffType 比較結果タイプ
+     * @param filePath ファイルパス
+     */
+    public void addList(CompareKbn diffType,
+                        String filePath) {
+        DiffBean diffBean = new DiffBean();
+        diffBean.setCompareKbn(diffType);
+        diffBean.setFilePath(filePath);
+        diffList.add(diffBean);
+    }
+
+    /**
+     * 比較結果格納リストを取得します。
+     * @return 比較結果格納リスト
+     */
+    public List<DiffBean> getDiffList() {
+        return diffList;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        diffList.forEach(diffBean -> {
+            sb.append(diffBean.getCompareKbn().value())
+              .append(",")
+              .append(diffBean.getFilePath())
+              .append(System.lineSeparator());
+        });
+        return sb.toString();
+    }
+}
+
+class DiffBean {
+
+    /** 比較結果タイプ */
+    private CompareKbn compareKbn;
+
+    /** ファイルパス（相対パス） */
+    private String filePath;
+
+    /**
+     * 比較結果タイプを取得します。
+     * @return 比較結果タイプ
+     */
+    public CompareKbn getCompareKbn() {
+        return compareKbn;
+    }
+
+    /**
+     * 比較結果タイプを設定します。
+     * @param compareKbn 比較結果タイプ
+     */
+    public void setCompareKbn(CompareKbn compareKbn) {
+        this.compareKbn = compareKbn;
+    }
+
+    /**
+     * ファイルパス（相対パス）を取得します。
+     * @return ファイルパス（相対パス）
+     */
+    public String getFilePath() {
+        return filePath;
+    }
+
+    /**
+     * ファイルパス（相対パス）を設定します。
+     * @param filePath ファイルパス（相対パス）
+     */
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+}
